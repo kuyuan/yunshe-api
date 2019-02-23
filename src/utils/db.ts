b38dc1d0d74214c733c34754f6ea82dcd1feee3e
BranLiang <lby89757@hotmail.com>
2019-02-23 18:34:45 +0800

    Add db config

---
+++ src/utils/db.ts
@@ -0,0 +1,13 @@
+const rethinkdbdash = require('rethinkdbdash')
+
+var r = rethinkdbdash({
+  db: process.env.DATABASE_NAME,
+  max: 500,
+  buffer: 5,
+  timeoutGb: 60 * 1000,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  password: process.env.DATABASE_PASSWORD
+})
+
+module.exports = r
