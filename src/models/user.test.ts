7c83b133069665cf708fcc7016ba5a1f411ece85
BranLiang <lby89757@hotmail.com>
2019-02-28 20:16:36 +0800

    Fix the test

--- src/models/user.test.ts
+++ src/models/user.test.ts
@@ -2,13 +2,15 @@ import {
   WUQIAN_ID,
 } from "@support/seed/constants";
 import { getUserById } from "./user";
+import { r } from 'rethinkdb-ts';
+
+afterAll(async () => {
+  await r.getPoolMaster().drain()
+})
 
 describe("getUserById", () => {
-  test("it can get user data", async () => {
+  test("it get user data", async () => {
     const user = await getUserById(WUQIAN_ID);
     expect(user.id).toBe(WUQIAN_ID);
   });
-});
-test("dummy", () => {
-  expect(1 + 2).toBe(3);
 });
