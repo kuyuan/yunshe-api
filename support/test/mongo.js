ac10a36e6e760aaaa293b4ee64dc40910a722d94
BranLiang <lby89757@hotmail.com>
2019-03-03 00:02:35 +0800

    Use localhost instead

--- support/test/mongo.js
+++ support/test/mongo.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const createClient = () => {
-  return new MongoClient('mongodb://127.0.0.1:27017/yunsheTest', {
+  return new MongoClient('mongodb://localhost:27017/yunsheTest', {
     useNewUrlParser: true
   })
 }
