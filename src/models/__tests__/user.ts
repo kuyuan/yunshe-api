c3854399e281ee972e2e8b4390e375bd75c7e53c
BranLiang <lby89757@hotmail.com>
2019-03-04 19:03:43 +0800

    Update the user model

--- src/models/__tests__/user.ts
+++ src/models/__tests__/user.ts
@@ -1,9 +1,23 @@
 import { getUserById } from "@models/user";
 import { WUQIAN_ID } from "@support/seed/constants";
+import { createClient } from "@utils/mongo";
+import { Db } from "mongodb";
+
+let db: Db;
+const client = createClient();
+
+beforeAll(async () => {
+  await client.connect();
+  db = client.db();
+});
+
+afterAll(() => {
+  client.close();
+});
 
 describe("getUserById", () => {
   test("basic", async () => {
-    const user = await getUserById(WUQIAN_ID);
+    const user = await getUserById(WUQIAN_ID, db);
     expect(user.name).toBe("吴倩");
   });
 });
