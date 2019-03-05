32bf5e0c21433fde4ec2f13d8f3cbb081ffd7178
BranLiang <lby89757@hotmail.com>
2019-03-05 18:55:21 +0800

    Add first integration test

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,9 +1,10 @@
 import { getUserById } from "@models/user";
+import { ObjectID } from "mongodb";
 
 export default {
   Query: {
     user: async (_, { id }, { db }) => {
-      const user = await getUserById(id, db);
+      const user = await getUserById(new ObjectID(id), db);
       return user;
     },
   },
