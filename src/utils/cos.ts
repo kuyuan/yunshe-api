d823d489d008b7cb522852f45df76e178b661fa4
BranLiang <lby89757@hotmail.com>
2019-03-05 23:57:05 +0800

    Fix the lint

--- src/utils/cos.ts
+++ src/utils/cos.ts
@@ -1,20 +1,20 @@
+import { IFile, ResourceTypes } from "@utils/interfaces";
 import COS from "cos-nodejs-sdk-v5";
 import sanitize from "sanitize-filename";
 import uuidv4 from "uuid/v4";
-import { IFile, ResourceTypes } from "@utils/interfaces";
 
 const cos = new COS({
   SecretId: process.env.COS_SECRET_ID,
   SecretKey: process.env.COS_SECRET_KEY,
-})
+});
 
-const bucket = process.env.COS_BUCKET
-const region = process.env.COS_REGION || 'ap-shanghai'
+const bucket = process.env.COS_BUCKET;
+const region = process.env.COS_REGION || "ap-shanghai";
 
 export const uploadImage = async (
   file: IFile,
   entity: ResourceTypes,
-  id: string
+  id: string,
 ) => {
   const { filename, stream } = await file;
   const sanitized = sanitize(filename);
@@ -26,13 +26,13 @@ export const uploadImage = async (
       Bucket: bucket,
       Region: region,
       Key: key,
-      Body: stream
+      Body: stream,
     }, (err, data) => {
       if (err) {
         throw err;
       } else {
-        resolve(data)
+        resolve(data);
       }
-    })
-  })
-}
+    });
+  });
+};
