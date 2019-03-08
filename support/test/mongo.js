006a79991fa31a83a9435d8f97c73d3a440889f3
BranLiang <lby89757@hotmail.com>
2019-03-09 00:24:05 +0800

    Optimize the test mongo

--- support/test/mongo.js
+++ support/test/mongo.js
@@ -2,9 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const { url, port, db } = require("./config");
 
 const createClient = () => {
-  return new MongoClient(`mongodb://${url}:${port}/${db}`, {
-    useNewUrlParser: true
-  })
+  return MongoClient.connect(`mongodb://${url}:${port}/${db}`, { useNewUrlParser: true });
 }
 
 module.exports = createClient
