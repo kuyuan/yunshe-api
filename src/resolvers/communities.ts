dc4b51aa8e0dbe66aab1b21fa60382b87eb5707d
BranLiang <lby89757@hotmail.com>
2019-03-10 14:46:21 +0800

    Add loader types

--- src/resolvers/communities.ts
+++ src/resolvers/communities.ts
@@ -1,8 +1,9 @@
+import { ILoader } from "@utils/loader";
 import { ObjectID } from "mongodb";
 
 export default {
   Query: {
-    community: async (_, { id }, { loader }) => {
+    community: async (_, { id }, { loader }: { loader: ILoader }) => {
       const community = await loader.community.load(new ObjectID(id));
       return community;
     },
--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,8 +1,9 @@
+import { ILoader } from "@utils/loader";
 import { ObjectID } from "mongodb";
 
 export default {
   Query: {
-    user: async (_, { id }, { loader }) => {
+    user: async (_, { id }, { loader }: { loader: ILoader }) => {
       const user = await loader.user.load(new ObjectID(id));
       return user;
     },
