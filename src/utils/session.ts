b4fb0d78f2371c0d17eb1230e1a11e0304913451
BranLiang <lby89757@hotmail.com>
2019-03-11 17:55:26 +0800

    Update the test config

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,12 +1,6 @@
 import session from "cookie-session";
 import Keygrip from "keygrip";
 
-if (!process.env.SESSION_COOKIE_SECRET) {
-  throw new Error(
-    "You have to provide the SESSION_COOKIE_SECRET environment variable.",
-  );
-}
-
 export const keygrip = new Keygrip([process.env.SESSION_COOKIE_SECRET]);
 
 export default session({
