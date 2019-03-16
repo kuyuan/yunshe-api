531c8a0317a6c4667a220466c2282a85ed879109
BranLiang <lby89757@hotmail.com>
2019-03-16 19:47:03 +0800

    Add edit user tests

--- src/typeDefs/__tests__/users.ts
+++ src/typeDefs/__tests__/users.ts
@@ -3,7 +3,7 @@ import createLoader from "@utils/loader";
 import { createClient } from "@utils/mongo";
 import { schema } from "@utils/server";
 import { graphql } from "graphql";
-import { Db, MongoClient } from "mongodb";
+import { Db, MongoClient, ObjectID } from "mongodb";
 
 let db: Db;
 let context;
@@ -61,3 +61,53 @@ describe("Query user", () => {
     });
   });
 });
+
+describe("Mutation User", () => {
+  const testUserId = new ObjectID("5c8ca1074cca2a3a5570c704");
+
+  beforeEach(async () => {
+    await db.collection("users").insertOne({
+      _id: testUserId,
+      name: "TEST USER",
+      description: "Who am I?",
+      profilePhoto: "http://corran.cn/profile-1.jpg",
+      coverPhoto: "http://corran.cn/cover-1.jpg",
+    });
+  });
+
+  afterEach(async () => {
+    await db.collection("users").deleteOne({
+      _id: testUserId,
+    });
+  });
+
+  test("editUser", async () => {
+    const query = `
+      mutation($input: EditUserInput!) {
+        editUser(input: $input) {
+          _id
+          name
+          description
+        }
+      }
+    `;
+    const params = {
+      input: {
+        name: "TEST USER - updated",
+        description: "Who am I? - updated"
+      }
+    }
+    context.currentUser = {
+      _id: testUserId,
+      name: "TEST USER",
+    }
+    const { data } = await graphql(schema, query, rootValue, context, params);
+    expect(data).toEqual({
+      editUser: {
+        _id: testUserId.toString(),
+        name: "TEST USER - updated",
+        description: "Who am I? - updated"
+      },
+    });
+  })
+})
