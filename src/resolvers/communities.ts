d7def421f834ee37d0231a244f552954d203b781
BranLiang <lby89757@hotmail.com>
2019-04-03 07:46:33 +0800

    Replace the community resolver method

--- src/resolvers/communities.ts
+++ src/resolvers/communities.ts
@@ -1,9 +1,10 @@
+import { getCommunityById } from "@models/community";
 import { Context } from "@utils/interfaces";
 
 export default {
   Query: {
-    community: async (_, { id }, { prisma }: Context) => {
-      const community = await prisma.community({ id });
+    community: async (_, { id }, { currentUser }: Context) => {
+      const community = await getCommunityById(id, currentUser && currentUser.id);
       return community;
     },
   },
