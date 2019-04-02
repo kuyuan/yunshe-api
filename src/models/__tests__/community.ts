a2d84a5ec9c0c562594fdc5dd2d00c4452338afa
BranLiang <lby89757@hotmail.com>
2019-04-03 07:35:10 +0800

    Fix the style

--- src/models/__tests__/community.ts
+++ src/models/__tests__/community.ts
@@ -1,6 +1,6 @@
 import { Community, User, UserCommunity } from "@prisma/index";
-import prisma from "@utils/prisma";
 import { generateUniqUsername } from "@support/test/helpers";
+import prisma from "@utils/prisma";
 import { getCommunityById } from "../community";
 
 let user: User;
@@ -14,27 +14,27 @@ beforeAll(async () => {
     name: "test user",
     username: generateUniqUsername(),
     description: "nothing to say",
-  })
+  });
   publicCommunity = await prisma.createCommunity({
     name: "测试社区",
     description: "这是一个测试社区",
     coverPhoto: "https://yunshe-sample-1256437689.cos.ap-shanghai.myqcloud.com/cover/cover1.jpg",
     profilePhoto: "https://yunshe-sample-1256437689.cos.ap-shanghai.myqcloud.com/community/comm1.png",
     isPrivate: false,
-  })
+  });
   privateCommunity = await prisma.createCommunity({
     name: "私人社区",
     description: "这是一个私人社区",
     coverPhoto: "https://yunshe-sample-1256437689.cos.ap-shanghai.myqcloud.com/cover/cover2.jpg",
     profilePhoto: "https://yunshe-sample-1256437689.cos.ap-shanghai.myqcloud.com/community/comm2.png",
     isPrivate: true,
-  })
-})
+  });
+});
 
 afterAll(async () => {
   await prisma.deleteManyCommunities({ id_in: [publicCommunity.id, privateCommunity.id] });
   await prisma.deleteManyUsers({ id: user.id });
-})
+});
 
 describe("getCommunityById", () => {
   test("when useCommunity exist it return community data", async () => {
@@ -42,20 +42,20 @@ describe("getCommunityById", () => {
       userId: user.id,
       communityId: privateCommunity.id,
       status: "ACTIVE",
-      role: "OWNER"
-    })
+      role: "OWNER",
+    });
     const result = await getCommunityById(privateCommunity.id, user.id);
-    expect(result.name).toBe("私人社区")
+    expect(result.name).toBe("私人社区");
     await prisma.deleteManyUserCommunities({ id: userCommunity.id });
-  })
+  });
 
   test("when user not connected it return null", async () => {
     const result = await getCommunityById(privateCommunity.id, user.id);
-    expect(result).toBe(null)
-  })
+    expect(result).toBe(null);
+  });
 
   test("when community is public, it return data", async () => {
     const result = await getCommunityById(publicCommunity.id, user.id);
-    expect(result.name).toBe("测试社区")
-  })
-})
+    expect(result.name).toBe("测试社区");
+  });
+});
